d68204f73a27d7ef939c81b7446e714075e7dc1f
Emma Shiller <emmashiller@gmail.com>
2021-03-08 12:58:07 +0200

    add rendering cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,14 @@
-let popup = document.querySelector(".popup");
+let popup = ['.popup popup_edit', '.popup popup_add', '.popup popup_image'];
 let showPopupButton = document.querySelector(".profile__edit");
 let closePopupButton = document.querySelector(".popup__close");
 let Username = document.querySelector(".profile__name");
 let Userjob = document.querySelector(".profile__job");
 let nameInput = document.querySelector("#username");
 let jobInput = document.querySelector("#userjob");
+const container = document.querySelector('.elements__list');
+const addCard = document.querySelector('#popupFormAdd');
+const templateElement = document.querySelector('.template');
+
 
 function showPopup() {
     nameInput.value = Username.textContent;
@@ -35,30 +39,39 @@ function formSubmitHandler(evt) {
 
 formElement.addEventListener("submit", formSubmitHandler);
 
+function createNewCard(element) {
+const newCard = templateElement.content.cloneNode(true);
+const titleCard = newCard.querySelector('.element__title');
+const linkCard = newCard.querySelector('.element__image');
+titleCard.textContent = element.name;
+linkCard.src = element.link;
 
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
+return newCard;
+}
+
+function renderList() {
+    const result = initialCards.map(createNewCard);
+
+    container.append(...result);
+}
+
+function addCardFormListener(evt) {
+  evt.preventDefault();
+  const inputName = addCard.querySelector('#placeName');
+  const inputPlace = addCard.querySelector('#placeLink');
+  const placeName = inputName.value;
+  const placeLink = inputPlace.value;
+
+ const newCardName = createNewCard({name: placeName});
+ const newCardLink = createNewCard({link: placeLink});
+
+ container.prepend(newCardName, newCardLink);
+
+ inputName.value = '';
+ inputPlace.value = '';
+ 
+}
+
+
+renderList();
+addCard.addEventListener('submit', addCardFormListener);
